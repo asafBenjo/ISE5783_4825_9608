13b17ca28af2fa5ff4f5cb55e6f17a32e3f2a3dd
asafBenjo <assafbango10@gmail.com>
2023-03-26 20:16:32 +0300

    stage1 process part 2

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -51,14 +51,7 @@ public class Vector extends Point{
     }
     public Vector crossProduct(Vector v)
     {
+        return new Vector ( xyz.d2*v.xyz.d3-xyz.d3*v.xyz.d2,xyz.d3*v.xyz.d1-xyz.d1*v.xyz.d3 , xyz.d1*v.xyz.d2-xyz.d2*v.xyz.d1);
+    }
 
-    }
-    public double lengthSquared()
-    {
-
-    }
-    public double length()
-    {
-throw new RuntimeException("not implemented");
-    }
 }
