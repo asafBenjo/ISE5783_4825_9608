c15e95130287398febcfb543089ac27903c0ea3a
asafBenjo <assafbango10@gmail.com>
2023-03-26 18:31:15 +0300

    stage1 process part 2

--- src/primitives/Double3.java
+++ src/primitives/Double3.java
@@ -6,11 +6,11 @@ import static primitives.Util.isZero;
  * @author Dan Zilberstein */
 public class Double3 {
     /** First number */
-    final double                d1;
+     double                d1;
     /** Second number */
-    final double                d2;
+     double                d2;
     /** Third number */
-    final double                d3;
+     double                d3;
 
     /** Zero triad (0,0,0) */
     public static final Double3 ZERO = new Double3(0, 0, 0);
